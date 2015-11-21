f30b7db25c83f6a56b235b0bf5600eecbdaab684
Justin Leo Ye <justinleoye@gmail.com>
2015-11-21 23:42:35 +0800

    add env

---
+++ env/production.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: staging.sh
+# Author: Justin Leo Ye
+# mail: justinleoye@gmail.com
+# Created Time: Sat Nov 21 23:40:21 2015
+#########################################################################
+#!/bin/bash
+
+export MONGO_URI="mongodb://localhost:27017/"
+
+export MONGO_DATABASE="o2o_info"
